68efeaaf7c55683e54050c74689d976892738da6
rpraveenverma <rpraveen@cse.iitb.ac.in>
2014-04-17 11:54:47 +0530

    Update raft_client.go

--- eval/raft_client.go
+++ eval/raft_client.go
@@ -6,10 +6,15 @@ import (
 )
 
 func main() {
-	raft.New(101,5, "configurationfile.json")
-	raft.New(102,40, "configurationfile.json")
-	raft.New(103, 40,"configurationfile.json")
-	raft.New(104, 40,"configurationfile.json")
-	raft.New(105, 40,"configurationfile.json")
-	time.Sleep(50*time.Second)
+	raft.New(101,"configurationfile.json")
+	raft.New(102,"configurationfile.json")
+	raft.New(103,"configurationfile.json")
+	raft.New(104,"configurationfile.json")
+	raft.New(105,"configurationfile.json")
+	raft.New(106,"configurationfile.json")
+	raft.New(107,"configurationfile.json")
+	raft.New(108,"configurationfile.json")
+	raft.New(109,"configurationfile.json")
+	raft.New(110,"configurationfile.json")
+	time.Sleep(time.Second*5000)
 }
